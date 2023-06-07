921a9961288eb8adc0d2fdbf91a9dddd8f6e7ff4
DucTato <65166409+DucTato@users.noreply.github.com>
2023-06-07 22:17:28 +0700

    Camera Look & Zoom
    Added Camera functions

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,6 +6,8 @@ public class CameraController : MonoBehaviour
 {
     public Camera mainCamera;   
     public static CameraController instance;
+    public Transform target;
+    [SerializeField] private float camSpeed;
     private void Awake()
     {
         instance = this;
@@ -13,12 +15,27 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = PlayerController.instance.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Following the player
         
+        if (Input.GetMouseButton(1))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+             // Move the Camera
+             transform.position = Vector3.Lerp(transform.position, new Vector3(hit.point.x, hit.point.y, -10f), Time.deltaTime * camSpeed);
+            }
+        }
+        else if (target != null)
+        {
+            transform.position = new Vector3(target.position.x, target.position.y, -10f);
+        }
     }
 }
